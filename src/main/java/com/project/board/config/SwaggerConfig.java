a46552bb86ddb11947beeb52e8c3abec04a6836d
Kkyehit <rp01020113994@gmail.com>
2020-07-23 10:25:11 +0900

    dev 환경에서만 swagger설정및 rediection 활성화

--- src/main/java/com/project/board/config/SwaggerConfig.java
+++ src/main/java/com/project/board/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.project.board.config;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,6 +14,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Profile("dev")
 public class SwaggerConfig {
 
     ApiInfo apiInfo(){
