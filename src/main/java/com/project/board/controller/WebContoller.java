0e172639106a5a4bdfa184f3c9cbdd01b91551ab
unknown <Administrator@SKCC18N00002.SKCC.NET>
2020-11-18 15:05:14 +0900

    Web Controller 추가

--- src/main/java/com/project/board/controller/WebContoller.java
+++ src/main/java/com/project/board/controller/WebContoller.java
@@ -1,2 +1,34 @@
-package com.project.board.controller;public class WebContoller {
+package com.project.board.controller;
+
+import com.project.board.model.request.AddBoardModel;
+import com.project.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/web")
+public class WebContoller {
+    @Autowired
+    BoardService boardService;
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(){
+        return "index.html";
+    }
+
+    @RequestMapping(value = "/board", method = RequestMethod.POST)
+    public String insertBoard(@ModelAttribute AddBoardModel addBoardModel){
+        boardService.addBoard(addBoardModel);
+        return "redirect:/web/";
+    }
+    @RequestMapping(value = "/board", method = RequestMethod.GET)
+    public String boardPage(Model model){
+        model.addAttribute("addBoardModel", new AddBoardModel());
+        return "boardInsertPage.html";
+    }
 }
