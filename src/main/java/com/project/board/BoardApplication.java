9b6aa1b7a2f1db62dc874e9c074ef416f66adcc7
unknown <Administrator@SKCC18N00002.SKCC.NET>
2020-11-18 15:01:45 +0900

    Locale 설정

--- src/main/java/com/project/board/BoardApplication.java
+++ src/main/java/com/project/board/BoardApplication.java
@@ -2,7 +2,12 @@ package com.project.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class BoardApplication {
@@ -10,4 +15,10 @@ public class BoardApplication {
 		SpringApplication.run(BoardApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver(){
+		SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+		sessionLocaleResolver.setDefaultLocale(Locale.US);
+		return sessionLocaleResolver;
+	}
 }
