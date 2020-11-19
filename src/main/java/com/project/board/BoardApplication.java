84cf89127fb17811cc8539edfc50d2416b75b89a
unknown <Administrator@SKCC18N00002.SKCC.NET>
2020-11-19 14:06:41 +0900

    main class가 SpringBootServletInitializer 상속받는 것으로 변경 Servlet 3.0 스펙에서는  web.xml 없이 배포가 가능하게 되었고 Spring Framework는 Servlet 3.0 이상 환경에서 web.xml 대신하여 ServletContext를 프로그래밍적으로 다룰 수 있게 WebApplicationInitializer 인터페이스를 제공 한다.

--- src/main/java/com/project/board/BoardApplication.java
+++ src/main/java/com/project/board/BoardApplication.java
@@ -1,7 +1,10 @@
 package com.project.board;
 
+import com.project.board.service.BoardService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.LocaleResolver;
@@ -10,9 +13,14 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import java.util.Locale;
 
 @SpringBootApplication
-public class BoardApplication {
+public class BoardApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BoardApplication.class);
 	}
 
 	@Bean
